11778143870d312c1f948e8ede533155fd22600e
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-08-07 15:39:02 -0700

    removed nav component for temporary deployment

--- sorting-algo/app/components/NavBar.tsx
+++ sorting-algo/app/components/NavBar.tsx
@@ -15,24 +15,25 @@ const NavBar = (array: any, setArray: any, handleShuffle: void) => {
   };
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Sorting Algorithms</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li onClick={handleSelectionSort}>
-            <a>Selection Sort</a>
-          </li>
-          <li onClick={handleBubbleSort}>
-            <a>Bubble Sort</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end" onClick={handleShuffle}>
-        <a className="btn text-l hover:text-white">Shuffle</a>
-      </div>
-    </div>
+    // <div className="navbar bg-base-100">
+    //   <div className="navbar-start">
+    //     <a className="btn btn-ghost text-xl">Sorting Algorithms</a>
+    //   </div>
+    //   <div className="navbar-center hidden lg:flex">
+    //     <ul className="menu menu-horizontal px-1">
+    //       <li onClick={handleSelectionSort}>
+    //         <a>Selection Sort</a>
+    //       </li>
+    //       <li onClick={handleBubbleSort}>
+    //         <a>Bubble Sort</a>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   <div className="navbar-end" onClick={handleShuffle}>
+    //     <a className="btn text-l hover:text-white">Shuffle</a>
+    //   </div>
+    // </div>
+    <></>
   );
 };
 
