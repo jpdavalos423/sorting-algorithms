eb8f15232016f673f0ac670ba6ad83dcc1b10d26
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-08-07 14:18:27 -0700

    added bubble sort, selection sort, and shuffle functionality

--- sorting-algo/app/page.tsx
+++ sorting-algo/app/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 import { useState } from "react";
 import SortGraph from "./components/SortGraph";
+import { bubbleSort, selectionSort } from "./utils/sortingAlgorithms";
+import { shuffleData } from "./utils/shuffleData";
 
 export default function Home() {
-  const [array, setArray] = useState([50, 150, 100, 250, 200, 300]);
+  const [array, setArray] = useState<number[]>([
+    50, 150, 100, 350, 200, 300, 100, 0,
+  ]);
+
+  const handleShuffle = () => {
+    shuffleData(array, setArray);
+  };
+  const handleBubbleSort = () => {
+    bubbleSort(array, setArray);
+  };
+  const handleSelectionSort = () => {
+    selectionSort(array, setArray);
+  };
 
   return (
     <>
@@ -13,15 +27,19 @@ export default function Home() {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
+            <li onClick={handleSelectionSort}>
               <a>Selection Sort</a>
             </li>
-            <li>
+            <li onClick={handleBubbleSort}>
               <a>Bubble Sort</a>
             </li>
           </ul>
         </div>
-        <div className="navbar-end"></div>
+        <div className="navbar-end">
+          <a className="btn text-l hover:text-white" onClick={handleShuffle}>
+            Shuffle
+          </a>
+        </div>
       </div>
 
       <SortGraph array={array} />
--- sorting-algo/app/utils/sortingAlgorithms.ts
+++ sorting-algo/app/utils/sortingAlgorithms.ts
@@ -1,13 +1,35 @@
-export const bubbleSort = async (array : any, setArray : any, delay = 100) => {
-    let arr = array.slice();
-    let n = arr.length;
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-          setArray([...arr]);
-          await new Promise((resolve) => setTimeout(resolve, delay));
-        }
+export const bubbleSort = async (array: any, setArray: any, delay = 100) => {
+  let arr = array.slice();
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        setArray([...arr]);
+        await new Promise((resolve) => setTimeout(resolve, delay));
       }
     }
-  };
+  }
+};
+
+export const selectionSort = async (array: any, setArray: any, delay = 100) => {
+  let arr = array.slice();
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      setArray([...arr]); // Update state to trigger re-render
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+};
